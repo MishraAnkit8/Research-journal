660453260816b4f1767c10fa3722c4fadd0312cc
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-11-25 17:41:00 +0530

    Create adminC.js

---
+++ adminC.js
@@ -0,0 +1,10 @@
+// authController.js
+const path = require('path');
+
+// Show login page
+exports.showLogin = (req, res) => {
+    res.render('login_admin'); // Assuming you have a login.ejs file in the views folder
+};
+
+
+
